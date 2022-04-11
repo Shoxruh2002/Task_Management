c79d48bb28dac62659a50a8eae4b82538459c646
Shoxruh2002 <bekpulatovshoxruh9@gmail.com>
2022-04-11 18:38:24 +0500

    aaasssssssssss

--- src/main/java/uz/master/demotest/configs/security/UserDetailsService.java
+++ src/main/java/uz/master/demotest/configs/security/UserDetailsService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import uz.master.demotest.entity.auth.AuthUser;
 import uz.master.demotest.repositories.AuthUserRepository;
 
+import java.util.Optional;
+
 @Service
 public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
 
@@ -17,7 +19,11 @@ public class UserDetailsService implements org.springframework.security.core.use
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        AuthUser user = repository.getAuthUsersByUsernameAndDeletedFalse(username).orElseThrow(() -> {throw new UsernameNotFoundException("User Not Found");});
-        return new uz.master.demotest.configs.security.UserDetails(user);
+        Optional<AuthUser> optional = repository.getAuthUsersByUsernameAndDeletedFalse(username);
+        if (optional.isPresent()) {
+            return new uz.master.demotest.configs.security.UserDetails(optional.get());
+
+        }
+        throw new UsernameNotFoundException("User Not Found");
     }
 }
